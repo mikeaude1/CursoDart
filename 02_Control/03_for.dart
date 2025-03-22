f732f07158222966696ad7461270a6e3d66c881d
mikeaude1 <miguelaude@ymail.com>
2025-03-22 17:37:16 -0600

    si

--- 02_Control/03_for.dart
+++ 02_Control/03_for.dart
@@ -8,5 +8,8 @@ main() {
   int numero = int.parse(stdin.readLineSync() ?? '0');
   for (int i = 0; i < 10; i++) {
     print('$numero multiplicado por ${i + 1} es igual a ${numero * (i + 1)}');
+    if (i == 9) {
+      break;
+    }
   }
 }
