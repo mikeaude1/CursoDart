168e0b9f5a1d059c4d1b056827527aa307336695
mikeaude1 <miguelaude@ymail.com>
2025-03-22 17:33:56 -0600

    tabla de multiplicar

--- 02_Control/03_for.dart
+++ 02_Control/03_for.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+
 main() {
   for (int i = 0; i < 10; i++) {
-    print('Hola mundo $i!' + i.toString());
+    print('Hola mundo $i! ${2 + i}');
+  }
+  stdout.writeln('Cual es la Bsase de la tabla?');
+  int numero = int.parse(stdin.readLineSync() ?? '0');
+  for (int i = 0; i < 10; i++) {
+    print('$numero multiplicado por ${i + 1} es igual a ${numero * (i + 1)}');
   }
 }
